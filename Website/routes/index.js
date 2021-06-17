9798051212d18f02639b68f73b7d9d10d479165c
DBishop10 <55112683+DBishop10@users.noreply.github.com>
2021-06-17 14:51:54 -0400

    Update index.js

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -1,113 +1,99 @@
-<h1>MONGODB - EXERCISE</h1>
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+let mongoose = require('mongoose');
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+var url = 'mongodb+srv://dtbishop:testpass@data01.8o2pb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+var spectopic = '';
+var specdata = '';
 
-<section class="insert">
-    <h3>Insert Data</h3>
-    <form action="/insert" method="post">
-        <div class="input">
-            <label for="title">Title</label>
-            <input type="text" id="title" name="title">
-        </div>
-        <div class="input">
-            <label for="content">Content</label>
-            <input type="text" id="content" name="content">
-        </div>
-        <div class="input">
-            <label for="author">Author</label>
-            <input type="text" id="author" name="author">
-        </div>
-        <button type="submit">INSERT</button>
-    </form>
-</section>
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
 
-<section class="specify">
-    <h3>Specify Search</h3>
-    <form action="/specify" method="post">
-        <div class="dropdown">
-            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown link
-            </a>
+router.get('/get-data', function(req, res, next) {
+  var resultArray = [];
+  var query = {};
+  query[spectopic] = RegExp(specdata);
+  console.log(specdata);
+  console.log(spectopic);
+  mongo.connect(url, {useUnifiedTopology: true}, function(err, client) {
+    var db = client.db('Trainings');
+    assert.equal(null, err);
+    var cursor = db.collection('RawData1').find(query);
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      client.close();
+      res.render('index', {items: resultArray});
+    });
+  });
+});
 
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-        </ul>
-    </div>
-        <div class="input"></div>    
-            <label for="DTA">Data Topic Area</label>
-            <input type="text" id="DTA" name="DTA">
-        </div>
-        <div class="input">
-            <label for="SpefString">Specific String</label>
-            <input type="text" id="SpefString" name="SpefString">
-        </div>
-        <button type="submit">INSERT</button>
-    </form>
-</section>
+router.post('/insert', function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+  
+  mongo.connect(url,  {useUnifiedTopology: true}, function(err, client) {
+    var db = client.db('Trainings');
+    assert.strictEqual(null, err);
+    db.collection('User Inputs').insertOne(item, function(err, result) {
+      assert.strictEqual(null, err);
+      console.log('Item inserted');
+      client.close();
+    });
+  });
 
-<section class="get">
-    <h3>Get Data</h3>
-    <a href="/get-data">LOAD DATA</a>
-    <div>
-        {{# each items }}
-            <article class="item">
-                <div>Title: {{ this.Title }}</div>
-                <div>URL: {{ this.URL }}</div>
-                <div>Provider: {{ this.Provider }}</div>
-                <div>Cost: {{ this.Cost }}</div>
-                <div>Public/DOD: {{ this.[Public/DOD] }}</div>
-                <div>Time Commitment (Hours): {{ this.[Time Commitment (Hours)] }}</div>
-                <div>Certificate/Degree Program: {{ this.[Certificate/Degree Program] }}</div>
-                <div>Data Topic Area: {{ this.[Data Topic Area] }}</div>
-                <div>Programming Language: {{ this.[Programming Language] }}</div>
-                <div>Base of Operations: {{ this.[Base of Operations] }}</div>
-                <div>Barrier to Entry: {{ this.[Barrier to Entry] }}</div>
-                <div>Self Paced vs Instructor Led: {{ this.[Self Paced vs Instructor Led] }}</div>
-                <div>Learning type: {{ this.[Learning type] }}</div>
-                <div>In person vs Remote: {{ this.[In person vs Remote] }}</div>
-                <div>Useful Training?: {{ this.[Useful Training?] }}</div>
-                <div>Comment: {{ this.Comment }}</div>
-                <div>Topics for Graphing: {{ this.[Topics for Graphing] }}</div>
-                <div>ID: {{ this._id }}</div>
-                <br>
-            </article>
-        {{/each}}
-    </div>
-</section>
+  res.redirect('/');
+});
 
-<section class="update">
-    <h3>Update Data</h3>
-    <form action="/update" method="post">
-        <div class="input">
-            <label for="id">ID</label>
-            <input type="text" id="id" name="id">
-        </div>
-        <div class="input">
-            <label for="title">Title</label>
-            <input type="text" id="title" name="title">
-        </div>
-        <div class="input">
-            <label for="content">Content</label>
-            <input type="text" id="content" name="content">
-        </div>
-        <div class="input">
-            <label for="author">Author</label>
-            <input type="text" id="author" name="author">
-        </div>
-        <button type="submit">UPDATE</button>
-    </form>
-</section>
-</section>
+router.post('/specify', function(req, res, next) {
+  spectopic = req.body.DTA;
+  specdata = req.body.SpefString;
+  res.redirect('/');
+});
 
-<section class="delete">
-    <h3>Delete Data</h3>
-    <form action="/delete" method="post">
-        <div class="input">
-            <label for="id">ID</label>
-            <input type="text" id="id" name="id">
-        </div>
-        <button type="submit">DELETE</button>
-    </form>
-</section>
+
+router.post('/update', function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+  var id = req.body.id;
+
+  mongo.connect(url, function(err, client) {
+    var db = client.db('Trainings');
+    assert.equal(null, err);
+    db.collection('User Inputs').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item updated');
+      client.close();
+    });
+  });
+  res.redirect('/');
+});
+
+router.post('/delete', function(req, res, next) {
+  var id = req.body.id;
+
+  mongo.connect(url, function(err, client) {
+    var db = client.db('Trainings');
+    assert.equal(null, err);
+    db.collection('User Inputs').deleteOne({"_id": objectId(id)}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item deleted');
+      client.close();
+    });
+  });
+  res.redirect('/');
+});
+
+module.exports = router;
