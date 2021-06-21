f7550807b4d5a56fbd687a77a5baaf2ab2f3f9de
jhy-cyber <85193895+jhy-cyber@users.noreply.github.com>
2021-06-21 08:25:36 -0400

    Update app.js

--- Website/app.js
+++ Website/app.js
@@ -3,7 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var hbs = require('express-handlebars')
+var hbs = require('express-handlebars');
+
+var sanitize = require('mongo-sanitize');
+var clean = sanitize(req.params.username);
 
 var indexRouter = require('./routes/index');
 
