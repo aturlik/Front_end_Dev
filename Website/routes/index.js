7d818297cb9bdc1db605cfc109ddc641b67e1116
Ethan Tran <57920836+kosta1612@users.noreply.github.com>
2021-07-07 12:18:24 -0400

    Update index.js

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -27,7 +27,7 @@ router.get('/admin', async(req, res, next) => {
   mongo.connect(url, {useUnifiedTopology: true}, function(err, client) {
     var db = client.db('Trainings');
     assert.equal(null, err);
-    var cursor = db.collection('FormattedRawData').aggregate([{'$search': {"index" : "SearchFormatted", 'text': {'query': id,'path': {'wildcard': '*'}}}}]);
+    var cursor = db.collection('FormattedRawData').aggregate([{'$search': {"index" : "SearchFormatted", 'text': {'query': id,'path':"_id"}}}]);
     cursor.forEach(function(doc, err) {
       assert.equal(null, err);
       results.push(doc);
