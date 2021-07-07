2f6378026e7a527323908f50801c7f60bcd0e86c
Ethan Tran <57920836+kosta1612@users.noreply.github.com>
2021-07-07 10:50:42 -0400

    Update index.js

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -20,11 +20,11 @@ router.get('/insert', function(req, res, next) {
 router.get('/admin', async(req, res, next) => {
   var id = req.query.idsearch;
   if (id !=null) {
-    console.log(idsearch);
+    console.log(id);
     res.render('admin', {idsearch:id});
   }
   else {
-	console.log(idsearch);
+	console.log(id);
 	res.render('admin', {idsearch:null});
   }
 });
