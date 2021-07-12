08a8caef2f2a3696a11cf980a6defe753d56acee
Ethan Tran <57920836+kosta1612@users.noreply.github.com>
2021-07-12 11:29:25 -0400

    Update app.js

--- Website/app.js
+++ Website/app.js
@@ -24,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css',express.static(path.join(__dirname, 'public/css')));
 
 app.use('/', indexRouter);
 
