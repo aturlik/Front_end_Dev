fde57b33394c64eeaf2734626aa84da209647eef
David Bishop <david.bishop3@gmail.com>
2021-07-15 11:02:55 -0400

    Created a download the database button

--- Website/app.js
+++ Website/app.js
@@ -6,8 +6,12 @@ var logger = require('morgan');
 var hbs = require('express-handlebars');
 var sanitize = require('mongo-sanitize');
 const Handlebars = require('handlebars')
-
-
+var url = 'mongodb+srv://dtbishop:testpass@data01.8o2pb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'; /* Connection string for our specific cluster */
+const mongodb = require("mongodb").MongoClient;
+var mongo = require('mongodb').MongoClient;
+const fastcsv = require("fast-csv");
+const fs = require("fs");
+const ws = fs.createWriteStream("public/files/FullDatabase.csv");
 var indexRouter = require('./routes/index');
 
 
@@ -27,6 +31,22 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use('/', indexRouter);
 
 
+
+  mongo.connect(url,  {useUnifiedTopology: true}, function(err, client) {
+    var db = client.db('Trainings').collection("FormattedRawData")
+    .find({})
+    .toArray((err, data) => {
+      if (err) throw err;
+      fastcsv
+        .write(data, { headers: true })
+        .on("finish", function() {
+          console.log("Write to FullDatabase.csv successfully!");
+        })
+        .pipe(ws);
+      client.close();
+    });
+  });
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -44,5 +64,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
